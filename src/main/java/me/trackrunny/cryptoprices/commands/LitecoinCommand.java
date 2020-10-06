ebd58ffa0fbc9e4f2e4ce077c62c0c8b1f8c6f00
Tal <justtal@protonmail.com>
2020-10-06 14:14:44 -0500

    Add files via upload

--- src/main/java/me/trackrunny/cryptoprices/commands/LitecoinCommand.java
+++ src/main/java/me/trackrunny/cryptoprices/commands/LitecoinCommand.java
@@ -4,10 +4,10 @@ import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.data.CommandData;
 import cn.nukkit.utils.TextFormat;
-import com.fasterxml.jackson.databind.JsonNode;
-import me.duncte123.botcommons.web.WebUtils;
+import me.trackrunny.cryptoprices.Main;
 
 public class LitecoinCommand extends Command {
+
     public LitecoinCommand() {
         super(CommandData
                 .builder("litecoin")
@@ -17,18 +17,8 @@ public class LitecoinCommand extends Command {
 
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-        WebUtils.ins.getJSONObject("https://api.coincap.io/v2/rates/litecoin").async((json) -> {
-            if (!json.get("data").get("id").asText().equals("litecoin")) {
-                sender.sendMessage(String.format("%sSomething went wrong getting the current price.", TextFormat.RED));
-                return;
-            }
-
-            final JsonNode data = json.get("data");
-            final String price = data.get("rateUsd").asText();
-
-            sender.sendMessage(String.format("%sCurrent price of Litecoin: %s$%s", TextFormat.DARK_GRAY, TextFormat.BOLD, price.substring(0, price.length() - 14)));
-        });
-
+        sender.sendMessage(String.format("%sCurrent price of Litecoin: %s$%s", TextFormat.DARK_GRAY, TextFormat.BOLD, Main.litecoinPrice));
         return true;
     }
+
 }
